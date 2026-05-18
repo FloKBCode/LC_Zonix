d36525a0ef1a087d60dbe729d7975d139ee85c55
FloKBCode <florence.korebelle@ynov.com>
2026-05-18 11:44:42 +0200

    feat: server and client connect successfully in local

--- paperio/src/main_client.c
+++ paperio/src/main_client.c
@@ -1,18 +1,28 @@
 // main_client.c - point d'entree du client
 #include <stdio.h>
+#include <winsock2.h>
 #include "../include/client.h"
 #include "../include/constants.h"
 
 int main() {
+    // ouvre une console pour voir les messages
+    AllocConsole();
+    freopen("CONOUT\$", "w", stdout);
+    freopen("CONIN\$", "r", stdin);
+
     char ip[64];
     printf("IP du serveur : ");
-    scanf("%s", ip);
+    fgets(ip, sizeof(ip), stdin);
+    // enleve le \n a la fin
+    ip[strcspn(ip, "\n")] = 0;
+
     if (init_client(ip, SERVER_PORT) < 0) {
         printf("Connexion impossible\n");
+        Sleep(2000);
         return 1;
     }
-    printf("Appuie sur Entree pour quitter\n");
-    getchar(); getchar();
+    printf("Connecte ! Appuie sur Entree pour quitter\n");
+    getchar();
     disconnect_client();
     return 0;
 }
