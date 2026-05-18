c1e469010990ae0809827b1b9077bfcac9b13d13
FloKBCode <florence.korebelle@ynov.com>
2026-05-18 11:08:21 +0200

    feat: add constants.h and types.h

--- paperio/include/constants.h
+++ paperio/include/constants.h
@@ -1,1 +1,14 @@
-// constants.h - toutes les constantes du jeu (taille grille, port, fps...)
+// constants.h - toutes les constantes du jeu, modifier ici uniquement
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define GRID_W      80      // largeur de la grille en cases
+#define GRID_H      60      // hauteur de la grille en cases
+#define CELL_SIZE   10      // taille d'une case en pixels
+#define MAX_PLAYERS  4      // nombre max de joueurs
+#define SERVER_PORT  5555   // port TCP par defaut
+#define TICK_RATE    100    // ms entre chaque update du jeu (10 fps)
+#define WINDOW_W    (GRID_W * CELL_SIZE)   // 800px
+#define WINDOW_H    (GRID_H * CELL_SIZE)   // 600px
+
+#endif
--- paperio/include/types.h
+++ paperio/include/types.h
@@ -1,1 +1,31 @@
-// types.h - structs partagees : Player, GameState, Packet
+// types.h - structs partagees entre serveur et client
+#ifndef TYPES_H
+#define TYPES_H
+
+#include "constants.h"
+
+typedef enum { DIR_UP, DIR_DOWN, DIR_LEFT, DIR_RIGHT } Direction;
+
+typedef struct {
+    int x, y;           // position sur la grille
+    Direction dir;      // direction actuelle
+    int alive;          // 1 = vivant, 0 = mort
+    int score;          // % de territoire possede
+    int trail[GRID_W * GRID_H][2]; // cases de la trainee [x][y]
+    int trail_len;      // nombre de cases dans la trainee
+} Player;
+
+typedef struct {
+    Player players[MAX_PLAYERS];
+    int map[GRID_H][GRID_W]; // 0=libre, 1-4=ID proprietaire
+    int player_count;
+    int running;
+} GameState;
+
+// paquet envoye par le client au serveur : juste la direction
+typedef struct {
+    int player_id;
+    Direction dir;
+} Packet;
+
+#endif
